080f930c7f960afa5bc7342c0a02014213c305d6
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2024-12-06 14:30:50 +0100

    added coordinate parsing

--- PlatformCode/OpenMoverplatform/src/main.cpp
+++ PlatformCode/OpenMoverplatform/src/main.cpp
@@ -3,14 +3,17 @@
 
 #define motor1Pin 10
 #define motor2Pin 11  
-#define pwmCenter 127
+#define pwmCenter 128
 #define pwmMax 200
 #define pwmMin 50
 
 void emergencyStop();
 
+JsonArray coordinates;
+
 void setup(){
   Serial.begin(115200);
+  Serial.println("Starting");
   analogWrite(motor1Pin, pwmCenter);
   analogWrite(motor2Pin, pwmCenter);
 }
@@ -26,30 +29,22 @@ void loop(){
       emergencyStop();
     }
     int messageIntention = doc["intent"];
-    Serial.print("Intent: ");
-    Serial.println(messageIntention);
-    switch (messageIntention){
-      case 0:
-        Serial.println("0");
-        break;
-      case 1:
-        Serial.println("1");
-        break;
-      case 2:
-        Serial.println("2");
-        break;
-      case 3:
-        Serial.println("3");
-        break;
-      case 4:
-        Serial.println("4");
-        break;
-      default:
-        emergencyStop();
-        break;
+    if(messageIntention == 0){ 
+      //recieve array of coordinates
+      coordinates = doc["coordinates"];
+      Serial.println("recieved coordinates");
+    }
+    else if(messageIntention == 1){
+      //send the currently stored coordinates via json
+      serializeJson(coordinates, Serial);
+      Serial.println("Should have sent the coordinates");
+    }
+    else{
+      Serial.println("Invalid intent");
+      emergencyStop();
+    }
     }
   }
-}
 
 void emergencyStop(){
   Serial.println("Emergency Stop");
