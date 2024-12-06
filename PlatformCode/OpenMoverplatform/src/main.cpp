8fda79de4e6902cac977db64d03a771d07d3baef
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2024-12-06 12:38:28 +0100

    Platform basic communication

--- PlatformCode/OpenMoverplatform/src/main.cpp
+++ PlatformCode/OpenMoverplatform/src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define motor1Pin 10
+#define motor2Pin 11  
+#define pwmCenter 127
+#define pwmMax 200
+#define pwmMin 50
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void emergencyStop();
+
+void setup(){
+  Serial.begin(115200);
+  analogWrite(motor1Pin, pwmCenter);
+  analogWrite(motor2Pin, pwmCenter);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  //wait unitl json data is available on the serial port and then parse it when it is available
+  if(Serial.available()){
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, Serial);
+    if(error){
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.f_str());
+      emergencyStop();
+    }
+    int messageIntention = doc["intent"];
+    Serial.print("Intent: ");
+    Serial.println(messageIntention);
+    switch (messageIntention){
+      case 0:
+        Serial.println("0");
+        break;
+      case 1:
+        Serial.println("1");
+        break;
+      case 2:
+        Serial.println("2");
+        break;
+      case 3:
+        Serial.println("3");
+        break;
+      case 4:
+        Serial.println("4");
+        break;
+      default:
+        emergencyStop();
+        break;
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void emergencyStop(){
+  Serial.println("Emergency Stop");
+  analogWrite(motor1Pin, pwmCenter);
+  analogWrite(motor2Pin, pwmCenter);
+  while(true){};
 }
