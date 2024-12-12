741d7162c39f6489b1cac6021286d37a4949b1c6
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2024-12-12 11:07:01 +0100

    added helper

---
+++ PlatformCode/PlatformHelper/helper.py
@@ -0,0 +1,17 @@
+import serial
+
+# Configure the serial port
+ser = serial.Serial(
+    port=input("Bot Serial Port? :"),        # Replace with your port name
+    baudrate=115200,      # Set the baud rate
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1           # Set a timeout value
+)
+
+if ser.is_open:
+    print("1:")
+    print("2:")
+    print("3:")
+    input("Choice :")
