c77684341197b27ca3e48c2aa688f2ffe6b7826a
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2025-01-20 15:43:51 +0100

    added coordinate transmission to helper

--- PlatformCode/PlatformHelper/intent0.py
+++ PlatformCode/PlatformHelper/intent0.py
@@ -1,4 +1,13 @@
 import serial
+import parsekml
+import json
 
 def process(port):
-    print("processing 3")
+    kmlArray = parsekml.parse(input("KML file path? :"))
+    kmlArray.insert(0,len(kmlArray))
+    dataToSend = {"intent": 0, "coordinates": kmlArray}
+    dataToSend = json.dumps(dataToSend)
+    dataToSend = dataToSend.encode('utf-8')
+    print("Data to send: ", dataToSend)
+    port.write(dataToSend)
+    print("Data sent")
