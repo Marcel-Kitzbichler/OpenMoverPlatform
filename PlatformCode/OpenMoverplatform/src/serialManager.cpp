73a84bde8c24093f09024efe5b446e41648f0ba4
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2025-04-16 11:55:48 +0200

    added direct serial control

--- PlatformCode/OpenMoverplatform/src/serialManager.cpp
+++ PlatformCode/OpenMoverplatform/src/serialManager.cpp
@@ -7,8 +7,10 @@
 #include "BluetoothSerial.h"
 
 bool motorHandled = false;
+const double uSecPWM = ((1000000/pwmFreq)/pow(2,pwmResolution));
 
 void serialManager(void * pvParameters){
+    bool directMotorControlSerial = false;
     BluetoothSerial SerialBT;
     SerialBT.begin("OpenMoverPlatformBTSerial");
     int64_t coordinateTable[100];
@@ -42,6 +44,23 @@ void serialManager(void * pvParameters){
                 }
             }
 
+            else if(messageIntention == 3){
+                if(motorControlHandle != NULL){
+                    vTaskDelete(motorControlHandle);
+                }
+                motorHandled = doc["setStatus"].as<bool>();
+                directMotorControlSerial = doc["setStatus"].as<bool>();
+                ledcWrite(pwmChMotor1, (pwmCenter) / uSecPWM);
+                ledcWrite(pwmChMotor2, (pwmCenter) / uSecPWM);
+            }
+
+            else if(messageIntention == 4){
+                if(motorHandled && directMotorControlSerial){
+                    ledcWrite(pwmChMotor1, (pwmCenter + doc["leftPWM"].as<int>()) / uSecPWM);
+                    ledcWrite(pwmChMotor2, (pwmCenter + doc["rightPWM"].as<int>()) / uSecPWM);
+                }
+            }
+
             else{
                 emergencyStop();
             }
@@ -72,6 +91,23 @@ void serialManager(void * pvParameters){
                 if(!motorHandled){
                     motorHandled = true;
                     xTaskCreatePinnedToCore(wpManagerExec, "wpManagerExec", 10000,(void *) coordinateTable, 1, motorControlHandle, 1);
+                }
+            }
+
+            else if(messageIntention == 3){
+                if(motorControlHandle != NULL){
+                    vTaskDelete(motorControlHandle);
+                }
+                motorHandled = doc["setStatus"].as<bool>();
+                directMotorControlSerial = doc["setStatus"].as<bool>();
+                ledcWrite(pwmChMotor1, (pwmCenter) / uSecPWM);
+                ledcWrite(pwmChMotor2, (pwmCenter) / uSecPWM);
+            }
+
+            else if(messageIntention == 4){
+                if(motorHandled && directMotorControlSerial){
+                    ledcWrite(pwmChMotor1, (pwmCenter + doc["leftPWM"].as<int>()) / uSecPWM);
+                    ledcWrite(pwmChMotor2, (pwmCenter + doc["rightPWM"].as<int>()) / uSecPWM);
                 }
             }
 
