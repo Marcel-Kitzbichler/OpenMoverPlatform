663b67c65d1074a6aa32ac58378d48cba5e3f452
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2025-05-13 19:08:43 +0200

    added setpoints to motorset

--- PlatformCode/OpenMoverplatform/src/motorSet.cpp
+++ PlatformCode/OpenMoverplatform/src/motorSet.cpp
@@ -8,6 +8,9 @@ const double uSecPWM = ((1000000/pwmFreq)/pow(2,pwmResolution));
 float biasL = 1.0;
 float biasR = 1.0;
 
+int setPointL = 0;
+int setPointR = 0;
+
 void motorSetup() {
     ledcSetup(pwmChMotor1, pwmFreq, pwmResolution);
     ledcSetup(pwmChMotor2, pwmFreq, pwmResolution);
@@ -25,6 +28,7 @@ void setMotorL(int speed) {
     } else {
         ledcWrite(pwmChMotor1, pwmCenter / uSecPWM);
     }
+    setPointL = speed;
 }
 
 void setMotorR(int speed) {
@@ -35,6 +39,7 @@ void setMotorR(int speed) {
     } else {
         ledcWrite(pwmChMotor2, pwmCenter / uSecPWM);
     }
+    setPointR = speed;
 }
 
 void setMotorBias(float biasL, float biasR, bool saveToPreferences) {
@@ -47,4 +52,12 @@ void setMotorBias(float biasL, float biasR, bool saveToPreferences) {
         preferences.putFloat("biasR", biasR);
         preferences.end();
     }
+}
+
+int getMotorL() {
+    return setPointL;
+} 
+
+int getMotorR() {
+    return setPointR;
 }
