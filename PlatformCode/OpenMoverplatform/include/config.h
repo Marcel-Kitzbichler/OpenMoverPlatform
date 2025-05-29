483d5f2864ee8eacb1871badb9e588fc579499d9
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2025-05-29 23:57:02 +0200

    working com on esp devboard, untested hardware peripherals

--- PlatformCode/OpenMoverplatform/include/config.h
+++ PlatformCode/OpenMoverplatform/include/config.h
@@ -5,13 +5,20 @@
 #define pwmMin 1860
 #define pwmDeadband 50
 #define pwmFreq 50
-#define pwmResolution 16
+#define pwmResolution 14
 #define pwmChMotor1 0
 #define pwmChMotor2 1
 #define GPSInterval 920
 #define GPSBaud 9600
-#define magSDAPin 18
-#define magSCLPin 23
+#ifdef __ESP32D0WDQ6__
+    #define magSDAPin 18
+    #define magSCLPin 23
+#endif
+#ifdef __ESP32S3__
+    #define magSDAPin 7
+    #define magSCLPin 6
+#endif
 #define batteryADCPin 14
 #define MagCalibrationTime 20000
 #define MagCalibrationSpeed 20
+#define BTSerialBufferSize 512
